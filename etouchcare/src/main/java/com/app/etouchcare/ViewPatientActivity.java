94ef25917aafa65da92437b610626a88577dfe61
wenzhongzheng <andrew198712@gmail.com>
2016-11-04 15:22:42 -0400

    add condition page

--- etouchcare/src/main/java/com/app/etouchcare/ViewPatientActivity.java
+++ etouchcare/src/main/java/com/app/etouchcare/ViewPatientActivity.java
@@ -11,5 +11,6 @@ public class ViewPatientActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_patient);
+
     }
 }
