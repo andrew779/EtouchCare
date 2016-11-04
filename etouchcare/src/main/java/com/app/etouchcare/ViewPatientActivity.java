fc052d962ec48510c50a0103437a1ece7627104c
wenzhongzheng <andrew198712@gmail.com>
2016-11-04 15:52:23 -0400

    add Condition page

--- etouchcare/src/main/java/com/app/etouchcare/ViewPatientActivity.java
+++ etouchcare/src/main/java/com/app/etouchcare/ViewPatientActivity.java
@@ -4,21 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.app.etouchcare.R;
 
-public class ViewPatientActivity extends AppCompatActivity {
+public class ViewPatientActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_patient);
 
-<<<<<<< HEAD
-=======
+
         // Get a support ActionBar corresponding to this toolbar
         ActionBar ab = getSupportActionBar();
         // Enable the Up button
@@ -40,6 +41,23 @@ public class ViewPatientActivity extends AppCompatActivity {
         txtName.setTextSize(40);
         txtName.setText(intentExtra);
 
->>>>>>> 519030fd9054e1d28a86c1abf9ebcf0a77178537
+        Button btn_condition = (Button) findViewById(R.id.btn_condition);
+        btn_condition.setOnClickListener(this);
+
+
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()){
+            case R.id.btn_condition:
+                intent = new Intent(this, ConditionActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+    }
+
+
 }
