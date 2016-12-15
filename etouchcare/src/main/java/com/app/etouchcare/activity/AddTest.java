e026dbf32f88a394d2cacb77dc78f986a349d953
guilherme <gtmorais@gmail.com>
2016-12-14 20:32:00 -0500

    DatePicker

--- etouchcare/src/main/java/com/app/etouchcare/activity/AddTest.java
+++ etouchcare/src/main/java/com/app/etouchcare/activity/AddTest.java
@@ -1,5 +1,7 @@
 package com.app.etouchcare.activity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -10,8 +12,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.app.etouchcare.datamodel.Patients;
 import com.android.volley.RequestQueue;
@@ -31,6 +35,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 
 public class AddTest extends AppCompatActivity implements PatientLoadedListener.TrialsLoadedListener {
@@ -40,6 +45,10 @@ public class AddTest extends AppCompatActivity implements PatientLoadedListener.
     private ArrayList<HashMap<String,String>> trialsData;
     final String URL = "https://mapd2016.herokuapp.com/patienttests/";
     String json="";
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +59,16 @@ public class AddTest extends AppCompatActivity implements PatientLoadedListener.
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        //calender stuff
+        dateView = (TextView) findViewById(R.id.textViewDate);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        showDate(year, month+1, day);
+
 
         Intent i = getIntent();
         theOne = i.getParcelableExtra("Patient");
@@ -65,7 +84,7 @@ public class AddTest extends AppCompatActivity implements PatientLoadedListener.
             public void onClick(View v) {
                 Toast.makeText(AddTest.this, "Adding", Toast.LENGTH_SHORT).show();
                 EditText txtResults = (EditText) findViewById(R.id.editTextResults);
-                EditText txtData = (EditText) findViewById(R.id.editTextDate);
+                TextView txtData = (TextView) findViewById(R.id.textViewDate);
                 Spinner spnTrial = (Spinner) findViewById(R.id.spinnerTrials);
 
                 Test test = new Test();
@@ -83,6 +102,43 @@ public class AddTest extends AppCompatActivity implements PatientLoadedListener.
                 finish();
             }
         });
+    }
+
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view) {
+        showDialog(999);
+        Toast.makeText(getApplicationContext(), "ca",
+                Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // TODO Auto-generated method stub
+        if (id == 999) {
+            return new DatePickerDialog(this,
+                    myDateListener, year, month, day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new
+            DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker arg0,
+                                      int arg1, int arg2, int arg3) {
+                    // TODO Auto-generated method stub
+                    // arg1 = year
+                    // arg2 = month
+                    // arg3 = day
+                    showDate(arg1, arg2+1, arg3);
+                }
+            };
+
+    private void showDate(int year, int month, int day) {
+        dateView.setText(new StringBuilder().append(day).append("/")
+                .append(month).append("/").append(year));
     }
 
     @Override
